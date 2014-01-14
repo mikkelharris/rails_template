9aa084d99dbd1a732036e653bbaee0196a77a110
Mikkel Harris <mikkelharris@Mikkels-MacBook-Pro.local>
2014-01-13 22:14:33 -0500

    Rails Template

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Template::Application.config.secret_key_base = '7ba6641c1ef49467f264bcc5a2c2270fc38160c0e99ad0f6007d0b4ef33223a10ba9b2990f79d56df8024106e2cfe4d049b3361ac7d8b2195e60b805c0432f04'
---
+++ vagrant/setup-nginx.sh
@@ -0,0 +1,12 @@
+sudo apt-get update
+sudo apt-get -y install nginx
+nginx -h
+cat /etc/init.d/nginx
+/etc/init.d/nginx -h
+sudo service nginx start
+cd /etc/nginx
+less nginx.conf
+cd sites-enabled
+sudo rm default
+sudo ln -s /vagrant/config/nginx.conf projectname
+sudo service nginx restart
